92afd5e2ac649cfa4f00112bd46ddf39aaea19dc
prothegee <prothegee@outlook.com>
2023-06-16 05:30:42 +0700

    adding more access

--- prthgdf/prthgdf.h
+++ prthgdf/prthgdf.h
@@ -12,16 +12,16 @@
 #define prthgdf_H
 #include <drogon/drogon.h>
 
-#include "prthgdf/LanguageContext.h"
-
 #include "prthgdf/HostAndUrl.h"
+
+#include "prthgdf/LanguageContext.h"
 
 
 namespace prthgdf
 {
-    class prthgdf::CLanguageContext;
-
     class prthgdf::CHostAndUrl;
+
+    class prthgdf::CLanguageContext;
 
 
     /**
@@ -43,28 +43,53 @@ namespace prthgdf
         ~CPrthgDf();
 
 
-        struct SBackend
+        struct STools
         {
-            std::shared_ptr<prthgdf::CLanguageContext> LanguageContext = nullptr;
+            static inline std::shared_ptr<prthgdf::CHostAndUrl> HostAndUrl = std::make_shared<prthgdf::CHostAndUrl>();
+            static inline std::shared_ptr<prthgdf::CLanguageContext> LanguageContext = std::make_shared<prthgdf::CLanguageContext>();
         };
-        // internal backend pointers access
-        std::shared_ptr<SBackend> pBackend = nullptr;
+        // tools pointer access
+        static inline std::shared_ptr<STools> pTools = std::make_shared<STools>();
 
 
-        struct SDrogonFramework
-        {
-            std::shared_ptr<prthgdf::CHostAndUrl> HostAndUrl = nullptr;
-        };
-        // internal drogonframework pointers access
-        std::shared_ptr<SDrogonFramework> pDrogonFramework = nullptr;
-
-
+        /**
+         * @brief get int 420
+         * 
+         * @return int 
+         */
         int Int420();
+
+        /**
+         * @brief get current status from custom_config of custom_config
+         * 
+         * @note list:
+         * @note 0 = undefined
+         * @note 1 = development
+         * @note 2 = staging
+         * @note 3 = production
+         * 
+         * @return int 
+         */
+        int Status();
+
+
+        /**
+         * @brief check if staging from custom_config of Status
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool IsStaging();
+
+
+        std::string EnvDomain();
+        std::string EnvProject();
+        std::string EnvVersion();
     };
 
 
     // internal prthgdf global pointers access
-    static inline std::shared_ptr<prthgdf::CPrthgDf> gPtrPrthgDf = std::make_shared<prthgdf::CPrthgDf>();
+    static inline std::shared_ptr<prthgdf::CPrthgDf> gPtr = std::make_shared<prthgdf::CPrthgDf>();
 
 
 } // namespace prthgdf

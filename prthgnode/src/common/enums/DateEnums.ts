7e8ee9dba29bde6cd6fedc1877c22529f68b8678
prothegee <prothegee@outlook.com>
2023-07-07 17:09:16 +0700

    adding date enums

---
+++ prthgnode/src/common/enums/DateEnums.ts
@@ -0,0 +1,28 @@
+/**
+ * @file DateEnums.ts
+ * @author Prana Ron (prthg@proton.me)
+ * 
+ * @brief internal enums collection for Storage keyword/prefix
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ * @contributors:
+ * - Prana Ron @prothegee (prthg@proton.me)
+ * 
+*/
+/**
+ * @brief project enum for date and time format
+*/
+export enum EDateAndTimeFormat
+{
+    undefined = 0,
+    FULL_TIMESTAMP,
+    YYYYMMDD,
+    YYYY,
+    MM,
+    DD,
+    hhmmss,
+    hh,
+    mm,
+    ss,
+}

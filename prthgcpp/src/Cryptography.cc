920a4b161cdc2a7c081a46ad90653e87393beaf4
prothegee <prothegee@outlook.com>
2023-07-11 02:47:53 +0700

    adding general head guard if defined for classes

--- prthgcpp/src/Cryptography.cc
+++ prthgcpp/src/Cryptography.cc
@@ -1,23 +1,4 @@
 #include "prthgcpp/inc/Cryptography.h"
-#include "prthgcpp/inc/Utility.h"
-
-#include <cryptopp/ec2n.h>
-#include <cryptopp/hex.h>
-#include <cryptopp/modes.h>
-#include <cryptopp/files.h>
-#include <cryptopp/osrng.h>
-#include <cryptopp/cryptlib.h>
-#include <cryptopp/secblock.h>
-#include <cryptopp/filters.h>
-#include <cryptopp/hex.h>
-#include <cryptopp/words.h>
-
-#include <cryptopp/blake2.h>
-#include <cryptopp/scrypt.h>
-#include <cryptopp/sha.h>
-#include <cryptopp/gcm.h>
-#include <cryptopp/aes.h>
-#include <cryptopp/chacha.h>
 
 
 prthgcpp::CCryptography::CCryptography()

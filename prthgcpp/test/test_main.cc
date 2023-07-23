d1110a51f45e50b38dbd434e067126cb6b4fd637
prothegee <prothegee@outlook.com>
2023-07-23 12:47:16 +0700

    updating test

--- prthgcpp/test/test_main.cc
+++ prthgcpp/test/test_main.cc
@@ -3,182 +3,44 @@
 
 int main()
 {
-    int errorCount{0};
-
-
-    prthgcpp::gsPtr->SayHello();
-
-
-#pragma region cryptography
+    prthgcpp::CUtility util;
     prthgcpp::CCryptography crypt;
 
-    std::cout << "(MSG) - testing cryptography" << "\n";
 
-    std::string falseAlphanumInput = "myinput@somewhere.net";
-    std::string trueAlphanumInput = "alphanumeric123456789"; 
-
-    std::cout << "\n(MSG) - checking alphanum" << "\n";
-
-    if (prthgcpp::gsPtr->pUtility->InputIsAlphanumeric(falseAlphanumInput))
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - falseAlphanumInput should not alphanumeric\n";
-    }
-    if (!prthgcpp::gsPtr->pUtility->InputIsAlphanumeric(trueAlphanumInput))
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - trueAlphanumInput should alphanumeric\n";
-    }
+    std::string emailInput = "foo@bar.baz";
+    std::string secretMessage = "super secret message!";
+    std::string rawPassword = "password123", saltPassword = "!supersalt123";
 
 
-    std::cout << "\n(MSG) - generate alphanum" << "\n";
-
-    std::string alphanum1, alphanum2, alphanum3;
-    srand(time(0));
-    alphanum1 = prthgcpp::gsPtr->pUtility->GenerateAlphanumeric(8);
-    alphanum2 = prthgcpp::gsPtr->pUtility->GenerateAlphanumeric(8);
-    alphanum3 = prthgcpp::gsPtr->pUtility->GenerateAlphanumeric(8);
-
-    std::cout << "alphanum1: " << alphanum1 << "\n";
-    std::cout << "alphanum2: " << alphanum2 << "\n";
-    std::cout << "alphanum3: " << alphanum3 << "\n\n";
-
-    if (alphanum1 == alphanum2 || alphanum1 == alphanum3)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - alphanum1 should not same with alphanum2 or alphanum3\n";
-    }
-    if (alphanum2 == alphanum1 || alphanum2 == alphanum3)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - alphanum2 should not same with alphanum1 or alphanum3\n";
-    }
-    if (alphanum3 == alphanum1 || alphanum3 == alphanum2)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - alphanum3 should not same with alphanum1 or alphanum2\n";
-    }
+    // sha & blake
+    std::cout << "SHA1:\n" << crypt.GenerateSHA1(emailInput) << "\n";
+    std::cout << "SHA224:\n" << crypt.GenerateSHA224(emailInput) << "\n";
+    std::cout << "SHA256:\n" << crypt.GenerateSHA256(emailInput) << "\n";
+    std::cout << "SHA384:\n" << crypt.GenerateSHA384(emailInput) << "\n";
+    std::cout << "SHA384:\n" << crypt.GenerateSHA384(emailInput) << "\n";
+    std::cout << "SHA512:\n" << crypt.GenerateSHA512(emailInput) << "\n";
+    std::cout << "BLAKE2b:\n" << crypt.GenerateBLAKE2b(emailInput) << "\n\n";
 
 
-    std::cout << "\n(MSG) - checking letter case" << "\n";
-    std::string inputlowercase = "ajwdho326traiwbdoa8y3gr";
-    std::string inputuppercase = "AIWLT2387TRAWHDP97Q3TRS";
-
-    std::cout << inputlowercase << " to " << prthgcpp::gsPtr->pUtility->ForceInputToCaseOf(inputlowercase, prthgcpp::ECLetterCase::uppercase) << "\n";
-    std::cout << inputuppercase << " to " << prthgcpp::gsPtr->pUtility->ForceInputToCaseOf(inputuppercase, prthgcpp::ECLetterCase::lowercase) << "\n";
-
-
-    std::cout << "\n(MSG) - generate sha from unique@mail.com" << "\n";
-    std::string inputSha = "unique@mail.com";
-
-    std::string L40 = crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L40, 0);
-    std::cout << L40 << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L40, 1) << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L40, 2) << "\n\n";
-    if (L40.length() != 40)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - L40 length is not 40\n";
-    }
-
-    std::string L56 = crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L56, 0);
-    std::cout << L56 << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L56, 1) << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L56, 2) << "\n\n";
-    if (L56.length() != 56)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - L56 length is not 56\n";
-    }
-
-    std::string L64 = crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L64, 0);
-    std::cout << L64 << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L64, 1) << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L64, 2) << "\n\n";
-    if (L64.length() != 64)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - L64 length is not 64\n";
-    }
-
-    std::string L96 = crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L96, 0);
-    std::cout << L96 << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L96, 1) << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L96, 2) << "\n\n";
-    if (L96.length() != 96)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - L96 length is not 96\n";
-    }
-
-    std::string L128 = crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L128, 0);
-    std::cout << L128 << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L128, 1) << "\n";
-    std::cout << crypt.GenerateSHA(inputSha, prthgcpp::ECCryptShaLength::L128, 2) << "\n\n";
-    if (L128.length() != 128)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - L128 length is not 128\n";
-    }
+    // xchacha20
+    std::string ikXChaCha20 = "abcdefghijklmnopqrstuvwxyz123456", ivXChaCha20 = "abcdefghijklmnopqrstuvwx";
+    std::string encXChaCha20 = crypt.EncryptXChaCha20(secretMessage, ikXChaCha20, ivXChaCha20);
+    std::string decXChaCha20 = crypt.DecryptXChaCha20(encXChaCha20, ikXChaCha20, ivXChaCha20);
+    std::cout << "encXChaCha20:\n" << encXChaCha20 << "\n";
+    std::cout << "decXChaCha20:\n" << decXChaCha20 << "\n\n";
 
 
-    std::cout << "\n(MSG) - generate blake2b from unique@mail.com" << "\n";
-    std::cout << crypt.GenerateBLAKE2b(inputSha, 0) << "\n";
-    std::cout << crypt.GenerateBLAKE2b(inputSha, 1) << "\n";
-    std::cout << crypt.GenerateBLAKE2b(inputSha, 2) << "\n";
+    // cbc aes
+    int ikCbcAes = 123456789, ivCbcAes = 987654321;
+    std::string encCBCAES = crypt.EncryptCBCAES(secretMessage, ikCbcAes, ivCbcAes);
+    std::string decCBCAES = crypt.DecryptCBCAES(encCBCAES, ikCbcAes, ivCbcAes);
+    std::cout << "encCBCAES:\n" << encCBCAES << "\n";
+    std::cout << "decCBCAES:\n" << decCBCAES << "\n\n";
 
 
-    std::cout << "\n(MSG) - checking password hasher SCRYPT" << "\n";
-    std::string password = "mypassword123", salt = "supersalt123", passwordHash, passwordCheck;
-    passwordHash = crypt.GeneratePasswordHasher(password, prthgcpp::ECCryptHasherMode::SCRYPT, salt);
-    passwordCheck = crypt.GeneratePasswordHasher(password, prthgcpp::ECCryptHasherMode::SCRYPT, salt);
-    std::cout << "passwordHash:\n" << passwordHash << "\n\n";
-    std::cout << "passwordCheck:\n" << passwordCheck << "\n\n";
-    if (passwordHash != passwordCheck)
-    {
-        errorCount += 1;
-        std::cout << "(ERR) - password is not match\n";
-    }
-
-
-    int ik = 123456789, iv = 987654321;
-    std::string iks = "abcdefghijklmnopqrstuvwxyz123456", ivs = "abcdefghijklmnopqrstuvwx";
-
-    std::cout << "\n(MSG) - encrypt decrypt stream cipher CBC_AES" << "\n";
-    
-    std::string message1 = "secret message 1", messageEnc1, messageDec1;
-    {
-        messageEnc1 = crypt.StreamCipher(prthgcpp::ECCryptCipherMode::encrypt, prthgcpp::ECCryptStreamCipherMode::CBC_AES, message1, ik, iv, iks, ivs);
-        messageDec1 = crypt.StreamCipher(prthgcpp::ECCryptCipherMode::decrypt, prthgcpp::ECCryptStreamCipherMode::CBC_AES, messageEnc1, ik, iv, iks, ivs);
-        std::cout << message1 << "\n";
-        std::cout << messageEnc1 << "\n";
-        std::cout << messageDec1 << "\n";
-        if (messageDec1 != message1)
-        {
-            errorCount += 1;
-            std::cout << "(ERR) - decrypt fail CBC_AES\n";
-        }
-    }
-
-
-    std::cout << "\n(MSG) - encrypt decrypt stream cipher XChaCha20" << "\n";
-    std::string message2 = "secret message 2", messageEnc2, messageDec2;
-    {
-        messageEnc2 = crypt.StreamCipher(prthgcpp::ECCryptCipherMode::encrypt, prthgcpp::ECCryptStreamCipherMode::XChaCha20, message2, ik, iv, iks, ivs);
-        messageDec2 = crypt.StreamCipher(prthgcpp::ECCryptCipherMode::decrypt, prthgcpp::ECCryptStreamCipherMode::XChaCha20, messageEnc2, ik, iv, iks, ivs);
-        std::cout << message2 << "\n";
-        std::cout << messageEnc2 << "\n";
-        std::cout << messageDec2 << "\n";
-        if (messageDec2 != message2)
-        {
-            errorCount += 1;
-            std::cout << "(ERR) - decrypt fail XChaCha20\n";
-        }
-    }
-#pragma endregion
-
-
-    std::cout << "\n(MSG) - error count: " << errorCount << "\n";
+    // scrypt
+    std::string password = crypt.GenerateHasherSCRYPT(rawPassword, saltPassword);
+    std::cout << "password:\n" << password << "\n\n";
 
 
     return 0;
